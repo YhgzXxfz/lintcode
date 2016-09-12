d05f6085bd00b7191e6b4ab121b4f25c13f2a7f4
Qingfei.You <qingfeiyou@gmail.com>
2016-09-12 22:55:55 +0200

    feat(cpp): add solution for trailing zeros

---
+++ trailing_zeros.cpp
@@ -0,0 +1,18 @@
+ass Solution {
+    /*
+     * param n: As desciption
+     * return: An integer, denote the number of trailing zeros in n!
+     */
+    public long trailingZeros(long n) {
+        // write your code here
+        if (n < 0) return 0;
+        
+        long sum = 0;
+        while (n >= 5) {
+            sum += n/5;
+            n /= 5;
+        }
+        return sum;
+    }
+};
+
