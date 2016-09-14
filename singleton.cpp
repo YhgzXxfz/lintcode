98da85265e5fbdb6b29b86f3e7635dec178297de
Qingfei.You <qingfeiyou@gmail.com>
2016-09-14 20:32:06 +0200

    feat(cpp): add solution for singleton

---
+++ singleton.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    /**
+     * @return: The same instance of this class every time
+     */
+    static Solution* getInstance() {
+        // write your code here
+        static Solution * instance = new Solution();
+        return instance;
+    }
+    
+private:
+    Solution() {}
+};
+
