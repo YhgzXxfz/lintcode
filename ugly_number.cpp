c9617771e67803ae706b98a02845813f2c4777ae
Qingfei.You <qingfeiyou@gmail.com>
2016-09-20 21:53:45 +0800

    feat(cpp): add solution for ugly number

---
+++ ugly_number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /**
+     * @param num an integer
+     * @return true if num is an ugly number or false
+     */
+    bool isUgly(int num) {
+        // Write your code here
+        if (num < 1) return false;
+        
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return 1 == num;
+    }
+};
