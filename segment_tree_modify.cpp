af10f82813e00787f45f9597e0e0e6b5b9c352ae
Qingfei.You <qingfeiyou@gmail.com>
2016-11-06 10:36:10 +0100

    feat(cpp): add solution for segment tree modify

---
+++ segment_tree_modify.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition of SegmentTreeNode:
+ * class SegmentTreeNode {
+ * public:
+ *     int start, end, max;
+ *     SegmentTreeNode *left, *right;
+ *     SegmentTreeNode(int start, int end, int max) {
+ *         this->start = start;
+ *         this->end = end;
+ *         this->max = max;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     *@param root, index, value: The root of segment tree and 
+     *@ change the node's value with [index, index] to the new given value
+     *@return: void
+     */
+    void modify(SegmentTreeNode *root, int index, int value) {
+        // write your code here
+        if (!root || root->start > index || root->end < index) return;
+        
+        if (root->start == index && root->end == index) {
+            root->max = value;
+            return ;
+        }
+        
+        modify(root->left, index, value);
+        modify(root->right, index, value);
+        
+        int left_max = root->left ? root->left->max : INT_MIN;
+        int right_max = root->right ? root->right->max : INT_MIN;
+        root->max = max(left_max, right_max);
+    }
+};
+
