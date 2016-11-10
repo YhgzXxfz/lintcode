dd0a4a3ab4d33c3cc8cb2c59cc35f60cd2ba5f9f
Qingfei.You <qingfeiyou@gmail.com>
2016-11-10 22:55:20 +0100

    feat(cpp): add solution for sort colors ii

---
+++ sort_colors_ii.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    /**
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */    
+    void sortColors2(vector<int> &colors, int k) {
+        // write your code here
+        sort(colors.begin(), colors.end());
+    }
+};
+
