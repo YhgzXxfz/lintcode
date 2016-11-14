ee15676ed03a8a58d3bfc86f549d039bd891707a
Qingfei.You <qingfeiyou@gmail.com>
2016-11-14 10:22:12 +0100

    feat(cpp): add solution for maximum product subarray

---
+++ maximum_product_subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    /**
+     * @param nums: a vector of integers
+     * @return: an integer
+     */
+    int maxProduct(vector<int>& nums) {
+        // write your code here
+        int len = nums.size();
+        
+        int result = nums[0];
+        int max_prod = nums[0], min_prod = nums[0];
+        for (int i = 1; i < len; ++i) {
+            if (nums[i] < 0) swap(max_prod, min_prod);
+            
+            max_prod = max(nums[i], max_prod * nums[i]);
+            min_prod = min(nums[i], min_prod * nums[i]);
+            
+            result = max(result, max_prod);
+        }
+        return result;
+    }
+};
+
