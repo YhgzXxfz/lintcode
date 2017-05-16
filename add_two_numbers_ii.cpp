daf7eebbf08a7e6680f0e8a2c9e0efe2d0406110
Qingfei.You <qingfeiyou@gmail.com>
2017-05-16 20:42:48 +0200

    feat(cpp): add another solution for add two numbers ii

--- add_two_numbers_ii.cpp
+++ add_two_numbers_ii.cpp
@@ -6,6 +6,52 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// 1
+class Solution {
+public:
+    /**
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2 
+     */
+    ListNode *addLists2(ListNode *l1, ListNode *l2) {
+        // write your code here
+        if (!l1) return l2;
+        if (!l2) return l1;
+        
+        unordered_map<int, int> mp1, mp2;
+        int i = 1, j = 1;
+        while (l1) {
+            mp1[i++] = l1->val;
+            l1 = l1->next;
+        }
+        while (l2) {
+            mp2[j++] = l2->val;
+            l2 = l2->next;
+        }
+        
+        i--; j--;
+        ListNode *head = nullptr;
+        int carry = 0, a, b, sum;
+        while (i > 0 || j > 0 || carry != 0) {
+            a = i > 0 ? mp1[i] : 0;
+            b = j > 0 ? mp2[j] : 0;
+            sum = a+b+carry;
+            
+            auto node = new ListNode(sum%10);
+            node->next = head;
+            head = node;
+            
+            carry = sum/10;
+            i--; j--;
+        }
+        return head;
+    }
+};
+
+
+
+// 2
 class Solution {
 public:
     /**
