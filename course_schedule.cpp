e3864a1096af1881a13215a7a2715b765401c6e0
Qingfei.You <qingfeiyou@gmail.com>
2017-05-17 23:36:00 +0200

    feat(cpp): add a faster solution for course schedule

--- course_schedule.cpp
+++ course_schedule.cpp
@@ -1,3 +1,48 @@
+// 1
+class Solution {
+public:
+    /**
+     * @param numCourses a total of n courses
+     * @param prerequisites a list of prerequisite pairs
+     * @return true if can finish all courses or false
+     */
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<unordered_set<int>> graph(numCourses);
+        for (const auto& pre : prerequisites) {
+            graph[pre.second].insert(pre.first);
+        }
+        
+        vector<int> indegrees(numCourses);
+        for (const auto& node : graph) {
+            for (const auto& neighbor : node) {
+                indegrees[neighbor]++;
+            }
+        }
+        
+        queue<int> zero_degrees;
+        for (int i = 0; i < numCourses; ++i) {
+            if (indegrees[i] == 0) zero_degrees.push(i);
+        }
+        
+        for (int i = 0; i < numCourses; ++i) {
+            if (zero_degrees.empty()) return false;
+            
+            auto curr = zero_degrees.front();
+            zero_degrees.pop();
+            for (auto& neighbor : graph[curr]) {
+                --indegrees[neighbor];
+                if (indegrees[neighbor] == 0) zero_degrees.push(neighbor);
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+// 2
 class Solution {
 public:
     /**
