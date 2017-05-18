48ac2693ed11889580b76ca69970c7948c5091ef
Qingfei.You <qingfeiyou@gmail.com>
2017-05-18 22:40:03 +0200

    feat(cpp): add solution for convert binary search tree to doubly linked list

---
+++ convert_binary_search_tree_to_doubly_linked_list.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ * Definition of Doubly-ListNode
+ * class DoublyListNode {
+ * public:
+ *     int val;
+ *     DoublyListNode *next, *prev;
+ *     DoublyListNode(int val) {
+ *         this->val = val;
+           this->prev = this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param root: The root of tree
+     * @return: the head of doubly list node
+     */
+    DoublyListNode* bstToDoublyList(TreeNode* root) {
+        vector<int> nums;
+        dfs(root, nums);
+        return convertSortedArrayToDoublyLinkedList(nums);
+    }
+
+private:
+    void dfs(TreeNode* root, vector<int>& nums) {
+        if (!root) return ;
+        
+        dfs(root->left, nums);
+        nums.push_back(root->val);
+        dfs(root->right, nums);
+    }
+    
+    DoublyListNode* convertSortedArrayToDoublyLinkedList(vector<int>& nums) {
+        int len = nums.size();
+        if (len == 0) return nullptr;
+        
+        auto head = new DoublyListNode(nums[0]), prev = head, curr = head;
+        for (int i = 1; i < len; ++i) {
+            curr = new DoublyListNode(nums[i]);
+            prev->next = curr;
+            curr->prev = prev;
+            prev = curr;
+        }
+        return head;
+    }
+};
+
+
+
