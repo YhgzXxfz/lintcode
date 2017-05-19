2a387a87f8f54c26bb3a6389570d2a4d678bb07c
Qingfei.You <qingfeiyou@gmail.com>
2017-05-19 23:39:24 +0200

    refactor(cpp): replace hash map by array to accelerate for first position unique character

--- first_position_unique_character.cpp
+++ first_position_unique_character.cpp
@@ -1,3 +1,24 @@
+// array is faster than hash map
+class Solution {
+public:
+    /**
+     * @param s a string
+     * @return it's index
+     */
+    int firstUniqChar(string& s) {
+        int mp[512] = {};
+        for (const auto& c : s) {
+            mp[c]++;
+        }
+        
+        for (int i = 0; i < s.size(); ++i) {
+            if (mp[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
+
+
 class Solution {
 public:
     /**
