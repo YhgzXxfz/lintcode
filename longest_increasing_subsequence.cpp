25ae533fa59be7532319eed8731c49b74861d568
Qingfei.You <qingfeiyou@gmail.com>
2017-10-25 22:16:10 +0200

    fix(cpp): update solution for longest increasing subsequence

--- longest_increasing_subsequence.cpp
+++ longest_increasing_subsequence.cpp
@@ -6,27 +6,13 @@ public:
      */
     int longestIncreasingSubsequence(vector<int> nums) {
         // write your code here
-        vector<int> LIS;
-        
-        for (auto i : nums) {
-            insert(LIS, i);
+        vector<int> result;
+        for (auto num : nums) {
+            auto it = lower_bound(result.begin(), result.end(), num);
+            if (it == result.end()) result.emplace_back(num);
+            else *it = num;
         }
-        
-        return LIS.size();
-    }
-    
-    void insert(vector<int> & LIS, int target)
-    {
-        int left = 0, right = LIS.size() -1;
-        
-        while (left <= right) {
-            int mid = left + (right- left)/2;
-            if (LIS[mid] > target) right = mid-1;
-            else left = mid+1;
-        }
-        
-        if (left == LIS.size()) LIS.push_back(target);
-        else LIS[left] = target;
+        return result.size();
     }
 };
 
